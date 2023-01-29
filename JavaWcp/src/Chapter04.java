e6831f0cf309498a474574a763fcd62896746920
oga34 <abbc.kii34@ezweb.ne.jp>
2023-01-29 14:13:20 +0900

    Initial Commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String my_name = "みほ";
+		System.out.println(my_name);
+
+		my_name = "ありがとう";
+		System.out.println(my_name);
+
+		int number = 7;
+		System.out.println(number);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123456789L;
+		System.out.println(long1);
+		double double1 = 1.6;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 1.7f;
+		System.out.println(float1);
+		Integer int1 = 12;
+		System.out.println(int1);
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array1[3]);
+	}
+}
