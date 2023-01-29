3e12f6500ceef52800157f1d9f6e447083403cf4
oga34 <abbc.kii34@ezweb.ne.jp>
2023-01-29 16:42:51 +0900

    [Add]Ren

---
+++ JavaWcp/src/Ren.java
@@ -0,0 +1,10 @@
+
+public class Ren {
+	public static void main(String[] args) {
+		int ans = 1;
+		for (int i = 1; i < 9; i++) {
+			ans *= 2;
+			System.out.println( "2の" + i + "乗は" + ans + "です");
+		}
+	}
+}
