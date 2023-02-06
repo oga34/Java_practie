9fae3080ddcc48f8ecbdaeabc2a8b13921d403ce
oga34 <abbc.kii34@ezweb.ne.jp>
2023-02-06 10:26:29 +0900

    [Add]Sample2

---
+++ JavaWcp/src/Sample1.java
@@ -0,0 +1,11 @@
+
+public class Sample1 {
+	public static void sum(int x, int y) {
+		System.out.println("りんごとオレンジの合計金額は" + (x + y) + "円です");
+	}
+	public static void main(String[] args) {
+		int orange = 100;
+		int apple = 200;
+		sum(orange, apple);
+	}
+}
