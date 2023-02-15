e2df84af9a875abd1e90d105ccc5e170148c582d
oga34 <abbc.kii34@ezweb.ne.jp>
2023-02-16 00:21:58 +0900

    [Add]Sam

--- JavaWcp/src/Sam.java
+++ JavaWcp/src/Sam.java
@@ -1,12 +1,23 @@
+interface Speak{
+	void speak();
+}
+interface Hello extends Speak{
+	void hello();
+}
+
+class Dog implements Hello{
+	public void speak() {
+		System.out.println("わんわん");
+	}
+	public void hello() {
+		System.out.println("わんちゃんです");
+	}
+}
+
 class Sam {
 	public static void main(String[] args) {
-		Student Tarou = new Student();
-		Tarou.num = 10;
-		show(Tarou);
-		System.out.println(Tarou.num);
-	}
-	public static void show(Student x) {
-		System.out.println(x.num);
-		x.num = 20;
+		Dog Inu = new Dog();
+		Inu.speak();
+		Inu.hello();
 	}
 }
